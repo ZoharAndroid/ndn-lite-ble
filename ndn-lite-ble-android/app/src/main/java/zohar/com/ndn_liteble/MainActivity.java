73aa8ea1d295c3b32174859b075543e7085dd514
ZoharAndroid <1048132071@qq.com>
2019-06-26 21:07:32 +0800

    android 基本界面完成

--- ndn-lite-ble-android/app/src/main/java/zohar/com/ndn_liteble/MainActivity.java
+++ ndn-lite-ble-android/app/src/main/java/zohar/com/ndn_liteble/MainActivity.java
@@ -1,13 +1,82 @@
 package zohar.com.ndn_liteble;
 
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = findViewById(R.id.dl_main_activity);
+        Toolbar mToolbar = findViewById(R.id.tb_main_activity);
+        FloatingActionButton mFloatingButton = findViewById(R.id.floating_button_main_activity);
+        final NavigationView mNavView = findViewById(R.id.nv_main_activity);
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.navigation_menu);
+        }
+
+        // 侧滑栏
+        mNavView.setCheckedItem(R.id.item_bluetooth_navigation);
+        mNavView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+
+        // 悬浮按钮
+        mFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"悬浮点击按钮",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toobar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.toolbar_scan_qr_code:
+                break;
+            case R.id.toolbar_refresh:
+                break;
+            case android.R.id.home:
+                // 点击返回按钮
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            default:
+        }
+
+        return true;
     }
 }
